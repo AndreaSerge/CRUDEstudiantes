b7044fba2fedc0e67dc18c9b71c3a87f90610e7c
AndreaSerge <acserger@unal.edu.co>
2024-02-05 17:20:20 +0100

    prueba

--- src/main/java/com/example/entities/Estudiante.java
+++ src/main/java/com/example/entities/Estudiante.java
@@ -39,6 +39,7 @@ public class Estudiante implements Serializable {
     private int id;
     private String nombre;
     private String apellidos;
+    sdfghjklkiuytr
 
     // Como quiero que guarde los campos de tipo enum: llamar en String
     @Enumerated(EnumType.STRING)
