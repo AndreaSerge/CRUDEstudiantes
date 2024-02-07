e1809b713d87f86e5f13cf5cb1a1dc49afd89069
AndreaSerge <acserger@unal.edu.co>
2024-02-07 15:37:59 +0100

    add persistirCurso CursoService

--- src/main/java/com/example/services/CursoService.java
+++ src/main/java/com/example/services/CursoService.java
@@ -1,5 +1,18 @@
 package com.example.services;
 
-public interface CursoService {
+import java.util.List;
 
+import com.example.entities.Curso;
+import com.example.entities.Estudiante;
+
+public interface CursoService {
+      // Método que me devuelve una lista de cursos
+      public List<Curso> dameLosCursos();
+
+      // Método que da un departamento según el id del estudiante
+      public Curso dameUnCurso(int idEstudiante);
+  
+      //Método que guarda un curso
+      public void persistirCurso(Estudiante estudiante);
+  
 }
