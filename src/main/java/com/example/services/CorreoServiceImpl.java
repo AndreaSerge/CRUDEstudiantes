76cb4b5438fd3a7fa852476756594ff5ba86762d
AndreaSerge <acserger@unal.edu.co>
2024-02-07 13:01:00 +0100

    add dameCorreos CorreoServiceImpl

--- src/main/java/com/example/services/CorreoServiceImpl.java
+++ src/main/java/com/example/services/CorreoServiceImpl.java
@@ -1,5 +1,40 @@
 package com.example.services;
 
-public class CorreoServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.CorreoDao;
+import com.example.dao.EstudianteDao;
+import com.example.entities.Correo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CorreoServiceImpl implements CorreoService {
+
+    private final CorreoDao correoDao;
+    private final EstudianteDao estudianteDao;
+
+
+    @Override
+    public List<Correo> dameCorreos(int idEstudiante) {
+        // TODO Auto-generated method stub
+        return correoDao.findByEstudiante(estudianteDao.findById(idEstudiante).get());
+    }
+
+    @Override
+    public void eliminaCorreos(int idEstudiante) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminaCorreos'");
+    }
+
+    @Override
+    public void persistirCorreo(int idEstudiante, Correo correo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'persistirCorreo'");
+    }
 
 }
