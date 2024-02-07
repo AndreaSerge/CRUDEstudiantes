cd794554df60162331ee8ca3b0f52ef4812b90c3
AndreaSerge <acserger@unal.edu.co>
2024-02-07 17:27:07 +0100

    add TelefonoServiceImpl

--- src/main/java/com/example/services/TelefonoService.java
+++ src/main/java/com/example/services/TelefonoService.java
@@ -1,5 +1,19 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.entities.Telefono;
+
 public interface TelefonoService {
+
+      // Método que me devuelve todos los télefonos
+    public List<Telefono> telefonos(int idEstudiante);
+
+    // Método que borre los télefonos correspondientes a un estudiante
+    public void eliminarTelefonos(int idEstudiante);
+
+    // Método que guarde(persistir) télefono. Necesita el estudiante para el que quiero persistir
+    public void persistirTelefono(int idEstudiante, Telefono telefono);
+
 
 }
