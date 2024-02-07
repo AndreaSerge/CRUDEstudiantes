ce7cc97ae8ff7050db6dcaed6abc1f47f2063a19
AndreaSerge <acserger@unal.edu.co>
2024-02-07 17:03:54 +0100

    Add CursoServiceImpl

--- src/main/java/com/example/services/CursoService.java
+++ src/main/java/com/example/services/CursoService.java
@@ -3,16 +3,15 @@ package com.example.services;
 import java.util.List;
 
 import com.example.entities.Curso;
-import com.example.entities.Estudiante;
 
 public interface CursoService {
       // Método que me devuelve una lista de cursos
       public List<Curso> dameLosCursos();
 
       // Método que da un departamento según el id del estudiante
-      public Curso dameUnCurso(int idEstudiante);
+      public Curso dameUnCurso(int idCurso);
   
       //Método que guarda un curso
-      public void persistirCurso(Estudiante estudiante);
+      public void persistirCurso(Curso curso);
   
 }
--- src/main/java/com/example/services/CursoServiceImpl.java
+++ src/main/java/com/example/services/CursoServiceImpl.java
@@ -1,5 +1,38 @@
 package com.example.services;
 
-public class CursoServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.dao.CursoDao;
+import com.example.entities.Curso;
+import com.example.entities.Estudiante;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CursoServiceImpl implements CursoService {
+
+    private final CursoDao cursoDao;
+
+    @Override
+    public List<Curso> dameLosCursos() {
+        // TODO Auto-generated method stub
+        return cursoDao.findAll();
+    }
+
+    @Override
+    public Curso dameUnCurso(int idCurso) {
+        // TODO Auto-generated method stub
+       return cursoDao.findById(idCurso).get();
+    }
+
+    @Override
+    public void persistirCurso(Curso curso) {
+        // TODO Auto-generated method stub
+        cursoDao.save(curso);
+    }
+    
 
 }
