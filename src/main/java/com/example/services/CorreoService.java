dcde21113616f38bb25565dee230828d1288c0a5
AndreaSerge <acserger@unal.edu.co>
2024-02-07 12:46:16 +0100

    add methods CorreoService

--- src/main/java/com/example/services/CorreoService.java
+++ src/main/java/com/example/services/CorreoService.java
@@ -1,7 +1,15 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.entities.Correo;
+
 public interface CorreoService {
 
-    
+    public List<Correo> dameCorreos(int idEstudiante);
+
+    public void eliminaCorreos(int idEstudiante);
+
+    public void persistirCorreo(int idEstudiante, Correo correo);
 
 }
