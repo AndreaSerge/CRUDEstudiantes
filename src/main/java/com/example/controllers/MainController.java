7a09f723cf5134e33da84e0f0b09a7cb7d22067c
AndreaSerge <acserger@unal.edu.co>
2024-02-08 06:32:03 +0100

    add persistirEstudiante MainController

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,14 +1,23 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.entities.Correo;
 import com.example.entities.Estudiante;
+import com.example.entities.Telefono;
 import com.example.services.CursoService;
 import com.example.services.EstudianteService;
 
@@ -49,7 +58,7 @@ public class MainController {
             estudianteService.dameUnEstudiante(idEstudiante));
         return "views/estudianteDetalles";
     }
-    
+
     @GetMapping("/frmAltaModificacion")
     public String formularioAltaModificacionEstudiante(Model model) {
 
@@ -63,6 +72,52 @@ public class MainController {
         cursoService.dameLosCursos());
     
     return "views/frmAltaModificacionEstudiante";
+    }
+
+    @PostMapping("/persistir")
+    @Transactional // todas las que necesitan modificación en la base de datos, es el autocommit
+    public String persistirEstudiante(@ModelAttribute(name="estudiante") Estudiante estudiante,
+    @RequestParam(name ="numerosTel", required = false) String telefonosRecibidos,
+    @RequestParam(name = "direccionesCorreo", required = false) String correosRecibidos) {
+
+        // Procesar los telefonos
+        if(telefonosRecibidos != null) {
+            String[] arrayTelefonos = telefonosRecibidos.split(";");
+            List<String> numerosTelefonos = Arrays.asList(arrayTelefonos);
+
+            List<Telefono> telefonos = new ArrayList<>();
+
+            numerosTelefonos.stream()
+                .forEach(numeroTelefono -> {
+                    telefonos.add(
+                    Telefono.builder()
+                    .telefono(numeroTelefono)
+                    .estudiante(estudiante)
+                    .build());
+                });
+                estudiante.setTelefonos(telefonos);
+        }
+
+        // Procesar los correos
+        if(correosRecibidos != null) {
+            String[] arrayCorreos = correosRecibidos.split(";");
+            List<String> direccionesDeCorreo = Arrays.asList(arrayCorreos);
+
+            List<Correo> correos = new ArrayList<>();
+
+            direccionesDeCorreo.stream()
+                .forEach(direccionDeCorreo -> {
+                    correos.add(Correo.builder()
+                    .correo(direccionDeCorreo)
+                    .estudiante(estudiante)
+                    .build());
+                });
+                estudiante.setCorreos(correos);
+        }
+
+        estudianteService.persistirEstudiante(estudiante);
+
+       return "redirect:/all";
     }
 
 
